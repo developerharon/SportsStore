7c73010e7da28c8c7fd592c86a7b1d62a9e53344
Haron Kipkorir <developerharon@users.noreply.github.com>
2020-05-26 23:35:21 +0300

    created the basic application functionality

---
+++ SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+
+        public ProductController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        /// <summary>
+        /// Renders a view to display a complete list of the products in the repository.
+        /// </summary>
+        public ViewResult List() => View(repository.Products);
+    }
+}
