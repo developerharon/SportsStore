9ccd18db782a6c594726b1f5fa1dfa83ea70ab49
Haron Kipkorir <developerharon@users.noreply.github.com>
2020-05-27 00:09:08 +0300

    Created the database context class

---
+++ SportsStore/Models/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SportsStore.Models
+{
+    /// <summary>
+    /// it's the bridge between the application and EF Core and provides access to the application's data using model objects, will be used to read and write applications data
+    /// </summary>
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        /// <summary>
+        /// Provides acceess to the Product objects in the database
+        /// </summary>
+        public DbSet<Product> Products { get; set; }
+    }
+}
