092211e210313b226c8668604f0c804796677b22
Haron Kipkorir <developerharon@users.noreply.github.com>
2020-06-01 00:28:34 +0300

    Created the edit action method and did a unit test to assert it edits the product and cannot edit a nonexistent product

--- SportsStore.Tests/AdminControllerTests.cs
+++ SportsStore.Tests/AdminControllerTests.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using Castle.DynamicProxy;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
@@ -34,6 +35,54 @@ namespace SportsStore.Tests
             Assert.Equal("P1", result[0].Name);
             Assert.Equal("P2", result[1].Name);
             Assert.Equal("P3", result[2].Name);
+        }
+
+        [Fact]
+        public void Can_Edit_Product()
+        {
+            // Arrange - create the mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product { ProductId = 1, Name = "P1" },
+                new Product { ProductId = 2, Name = "P2" },
+                new Product { ProductId = 3, Name = "P3" }
+            }.AsQueryable<Product>());
+
+            // Arrange - create the controller
+            AdminController target = new AdminController(mock.Object);
+
+            // Act 
+            Product p1 = GetViewModel<Product>(target.Edit(1));
+            Product p2 = GetViewModel<Product>(target.Edit(2));
+            Product p3 = GetViewModel<Product>(target.Edit(3));
+
+            // Assert
+            Assert.Equal(1, p1.ProductId);
+            Assert.Equal(2, p2.ProductId);
+            Assert.Equal(3, p3.ProductId);
+        }
+
+        [Fact]
+        public void Cannot_Edit_Nonexistent_Product()
+        {
+            // Arrange - create the mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product { ProductId = 1, Name = "P1" },
+                new Product { ProductId = 2, Name = "P2" },
+                new Product { ProductId = 3, Name = "P3" }
+            }.AsQueryable<Product>());
+
+            // Arrange - create the controller
+            AdminController target = new AdminController(mock.Object);
+
+            // Act
+            Product result = GetViewModel<Product>(target.Edit(4));
+
+            // Assert
+            Assert.Null(result);
         }
 
         private T GetViewModel<T>(IActionResult result) where T : class
