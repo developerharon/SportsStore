fd259c9e27d3d5cf5fe73d3aa395b81eb330ff2e
Haron Kipkorir <developerharon@users.noreply.github.com>
2020-06-01 22:38:11 +0300

    Added model validation Implemented client-side validation, Creating the Create new product action in admin controller

--- SportsStore/Models/Product.cs
+++ SportsStore/Models/Product.cs
@@ -1,11 +1,24 @@
-﻿namespace SportsStore.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net.NetworkInformation;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace SportsStore.Models
 {
     public class Product
     {
         public int ProductId { get; set; }
+
+        [Required(ErrorMessage = "Please enter a product name")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter product description")]
         public string Description { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "PLease enter a positive price")]
         public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Please specify a category")]
         public string Category { get; set; }
     }
 }
