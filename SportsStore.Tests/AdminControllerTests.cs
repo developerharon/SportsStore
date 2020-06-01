8fcd10e27a9e0a35b686b8f62dc05c3ceec7757e
Haron Kipkorir <developerharon@users.noreply.github.com>
2020-06-01 23:16:14 +0300

    Created a unit test for the deleting of products functionality

--- SportsStore.Tests/AdminControllerTests.cs
+++ SportsStore.Tests/AdminControllerTests.cs
@@ -140,6 +140,31 @@ namespace SportsStore.Tests
             Assert.IsType<ViewResult>(result);
         }
 
+        [Fact]
+        public void Can_Delete_Valid_Products()
+        {
+            // Arrange - create a product
+            Product product = new Product { ProductId = 2, Name = "Test" };
+
+            // Arrange - create the mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product { ProductId = 1, Name = "P1" },
+                product,
+                new Product { ProductId = 3, Name = "P3" }
+            }.AsQueryable<Product>());
+
+            // Arrange - create the controller
+            AdminController target = new AdminController(mock.Object);
+
+            // Act - delete the product
+            target.Delete(product.ProductId);
+
+            // Assert - ensure that the repository delete method was called with the correct product
+            mock.Verify(m => m.DeleteProduct(product.ProductId));
+        }
+
         private T GetViewModel<T>(IActionResult result) where T : class
         {
             return (result as ViewResult)?.ViewData.Model as T;
