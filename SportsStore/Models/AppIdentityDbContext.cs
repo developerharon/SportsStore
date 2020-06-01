d2a30a1143195c9bf4aa1b5712547bc68d7ff809
Haron Kipkorir <developerharon@users.noreply.github.com>
2020-06-02 00:08:53 +0300

    Finished with the AppIdentity database context class

--- SportsStore/Models/AppIdentityDbContext.cs
+++ SportsStore/Models/AppIdentityDbContext.cs
@@ -4,7 +4,10 @@ using Microsoft.EntityFrameworkCore;
 
 namespace SportsStore.Models
 {
-    public class AppIdentityDbContext 
+    public class AppIdentityDbContext : IdentityDbContext<IdentityUser>
     {
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : base(options)
+        {
+        }
     }
 }
